6d3e37c5869b9bc3a79c5286590c698364d092f7
Matheus Souza <matheus.souza@zup.com.br>
2017-03-31 16:58:28 -0300

    Banco Funcionando

--- app/src/main/java/com/apimdb/adapter/MovieSavedAdapter.java
+++ app/src/main/java/com/apimdb/adapter/MovieSavedAdapter.java
@@ -2,16 +2,24 @@ package com.apimdb.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.apimdb.ExtendActivity;
 import com.apimdb.R;
 import com.apimdb.SavedActivity;
 import com.apimdb.domain.Filme;
+import com.apimdb.persistencia.Controller;
+import com.apimdb.persistencia.CreateDataBase;
 
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 /**
@@ -23,10 +31,12 @@ public class MovieSavedAdapter extends RecyclerView.Adapter<ViewHolderSaved> {
     private List<Filme> myList;
     private LayoutInflater myLayoutInflater;
     private Context context;
+    private Controller controller;
     public MovieSavedAdapter(List<Filme> l, Context c){
         myList = l;
         myLayoutInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         context = c;
+        controller = new Controller(context);
     }
     @Override
     public ViewHolderSaved onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -36,18 +46,48 @@ public class MovieSavedAdapter extends RecyclerView.Adapter<ViewHolderSaved> {
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolderSaved holder, int position) {
+    public void onBindViewHolder(final ViewHolderSaved holder, final int position) {
         holder.imMovie.setImageBitmap(myList.get(position).getImagem());
         holder.tvTitle.setText(myList.get(position).getTitle());
         holder.tvDescription.setText(myList.get(position).getPlot());
 
-  /*      holder.btnExpand.setOnClickListener(new View.OnClickListener() {
+        //Configurando Imagem para mandar a outra activity
+        Drawable drawable;
+        Bitmap bitmap;
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        drawable = holder.imMovie.getDrawable();
+        bitmap = ((BitmapDrawable)drawable).getBitmap();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, stream);
+        final byte[] bitMapData = stream.toByteArray();
+
+
+        holder.btnExpand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                context.startActivity(new Intent(, ExtendActivity.class));
+                Bundle bundle = new Bundle();
+
+                bundle.putString("title", myList.get(position).getTitle());
+                bundle.putString("infos",myList.get(position).toString());
+                bundle.putSerializable("image", bitMapData);
+                Intent intent = new Intent(context, ExtendActivity.class);
+                intent.putExtras(bundle);
+                context.startActivity(intent);
             }
         });
-        */
+
+        holder.btnRemover.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                final String where = CreateDataBase.tabela.TITLE + "=" + "'" + myList.get(position).getTitle() + "'";
+                controller.DeletaDados(CreateDataBase.NOME_TABELA, where);
+                Toast.makeText(context, "Removed!", Toast.LENGTH_SHORT).show();
+                ((SavedActivity) context).refresh();
+
+            }
+        });
     }
 
     @Override
